a63c0bae8bd18653ccf29fa8e8bd300f936fcc13
sKawashima <bysk.333@gmail.com>
2021-11-25 22:01:58 +0900

    Use nest route

--- app/routes/admin.tsx
+++ app/routes/admin.tsx
@@ -1,6 +1,11 @@
-import { Link, useLoaderData } from "remix";
+import { Link, Outlet, useLoaderData } from "remix";
 import { getPosts } from "~/post";
 import type { Post } from "~/post";
+import adminStyles from "~/styles/admin.css";
+
+export let links = () => {
+  return [{ rel: "stylesheet", href: adminStyles }];
+};
 
 export let loader = () => {
   return getPosts();
@@ -20,7 +25,9 @@ export default function Admin() {
           ))}
         </ul>
       </nav>
-      <main>...</main>
+      <main>
+        <Outlet />
+      </main>
     </div>
   );
 }
